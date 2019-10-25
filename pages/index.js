0b09e3c4f7d2d8176b92c7ae0588fadc39f50e8c
Eric Adamski <er.adamski@gmail.com>
2019-10-24 21:36:15 -0400

    Oops

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default () => (
   <div>
-    <h1>Hello #Hacktoberfest</h1>
+    <h1>Hello hashd #Hacktoberfest</h1>
     <Link href="/profile">
       <button>profile</button>
     </Link>
